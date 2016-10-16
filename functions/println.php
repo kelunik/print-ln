f9c87f8b9d2a64bfb94aa2fbb77f341c7547d44a
Niklas Keller <me@kelunik.com>
2016-10-16 22:32:26 +0200

    Reduce the number of function calls
    This makes it more performant. This also removes the if (false) and instead turns the second eval info "real" code.

--- functions/println.php
+++ functions/println.php
@@ -1,13 +1,13 @@
 <?php
 
 if (!function_exists("println")) {
-    if (version_compare(phpversion(), "5.6.0", ">=")) {
+    if (PHP_VERSION_ID <= 50600) {
         eval('
             function println(...$strings) {
                 foreach ($strings as $string) {
                     print $string;
 
-                    if (php_sapi_name() !== "cli") {
+                    if (PHP_SAPI !== "cli") {
                         print "<br>";
                     }
 
@@ -16,29 +16,23 @@ if (!function_exists("println")) {
             }
         ');
     } else {
-        eval('
-            function println() {
-                $strings = func_get_args();
-
-                foreach ($strings as $string) {
-                    print $string;
-
-                    if (php_sapi_name() !== "cli") {
-                        print "<br>";
-                    }
-
-                    print PHP_EOL;
-                }
-            }
-        ');
-    }
-
-    if (false) {
         /**
-         * ...To help with for PHPStorm static analysis
+         * Prints each argument into a separate line.
          *
          * @param string $strings,... Strings to print
          */
-        function println() {}
+        function println() {
+            $strings = func_get_args();
+
+            foreach ($strings as $string) {
+                print $string;
+
+                if (PHP_SAPI !== "cli") {
+                    print "<br>";
+                }
+
+                print PHP_EOL;
+            }
+        }
     }
 }
